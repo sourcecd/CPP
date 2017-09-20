e26c2164ce7ea615b5fd6f2bd040a063624e667d
wint <wintchester@gmail.com>
2017-09-20 10:34:22 +0300

    New

---
+++ 4.11-6.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Max length of local names 1024,2048,4096, or unlimited\n";
+    std::cout << "variable names can start from literal char, can contain _ , and digits\n";
+    return 0;
+}
